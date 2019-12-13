868531d90785aec0125041081aeb29a0f34e835d
GwonHeeJun <gwonhj9751@naver.com>
2019-12-14 02:32:31 +0900

    [CREATE] Create Main Container, Default Setting

---
+++ src/containers/MainContainer/MainContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "./MainContainer.scss";
+
+export default class MainContainer extends Component {
+    render() {
+        return (
+            <div className="main-container">
+                Main
+            </div>
+        )
+    }
+}
