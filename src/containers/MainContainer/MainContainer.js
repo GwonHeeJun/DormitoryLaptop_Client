2a1a2f3c17c6758cb64ed096e99e5772c14d2e26
GwonHeeJun <gwonhj9751@naver.com>
2019-12-14 23:00:11 +0900

    [UPDATE] Update Container, Import Header & Choose Components

--- src/containers/MainContainer/MainContainer.js
+++ src/containers/MainContainer/MainContainer.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
+import BackImage from 'assets/image/backImage.png';
 import "./MainContainer.scss";
+import Header from 'components/Header/Header';
+import Choose from 'components/Choose/Choose';
 
 export default class MainContainer extends Component {
     render() {
         return (
             <div className="main-container">
                 <div className="main-container__forms">
-
+                    <Header />
+                    <Choose />
                 </div>
                 <div className="main-container__image-box">
-
+                    <img src={BackImage} alt="BackGroundImage" />
                 </div>
             </div>
         )
