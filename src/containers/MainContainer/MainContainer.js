284f1e6db5c4ae916e6a529eb52057df59aaa09e
GwonHeeJun <gwonhj9751@naver.com>
2019-12-15 18:39:17 +0900

    [UPDATE] Update makeMainForms Func, Connect Redux Store

--- src/containers/MainContainer/MainContainer.js
+++ src/containers/MainContainer/MainContainer.js
@@ -1,21 +1,61 @@
-import React, { Component } from 'react'
-import BackImage from 'assets/image/backImage.png';
+import React, { Component } from "react";
+import BackImage from "assets/image/backImage.png";
 import "./MainContainer.scss";
-import Header from 'components/Header/Header';
-import Choose from 'components/Choose/Choose';
+import Header from "components/Header/Header";
+import Choose from "components/Choose/Choose";
+import { connect } from "react-redux";
 
-export default class MainContainer extends Component {
-    render() {
+class MainContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  makeMainForms() {
+    const { userType } = this.props;
+    switch (userType) {
+      case "":
         return (
-            <div className="main-container">
-                <div className="main-container__forms">
-                    <Header />
-                    <Choose />
-                </div>
-                <div className="main-container__image-box">
-                    <img src={BackImage} alt="BackGroundImage" />
-                </div>
-            </div>
-        )
+          <React.Fragment>
+            <Header />
+            <Choose />
+          </React.Fragment>
+        );
+      case "student":
+          return (
+              <React.Fragment>
+                  Student
+              </React.Fragment>
+          )
+      case "admin":
+          return (
+              <React.Fragment>
+                  Admin
+              </React.Fragment>
+          )
+      default:
+        break;
     }
+  }
+
+  render() {
+    return (
+      <div className="main-container">
+        <div className="main-container__forms">
+            {this.makeMainForms()}
+        </div>
+        <div className="main-container__image-box">
+          <img src={BackImage} alt="BackGroundImage" />
+        </div>
+      </div>
+    );
+  }
 }
+const mapStateToProps = state => {
+  return {
+    userType: state.menu.userType
+  };
+};
+
+export default connect(mapStateToProps, null)(MainContainer);
