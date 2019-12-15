edf0711d4e29e47a832cc17ca02d45929d1b9f81
GwonHeeJun <gwonhj9751@naver.com>
2019-12-15 18:29:26 +0900

    [CREATE] Create Menu Store, Management Header & Choose Menu

---
+++ src/store/Menu/Menu.store.js
@@ -0,0 +1,32 @@
+export const CHANGE_USER_TYPE = "CHANGE_USER_TYPE";
+
+const initialState = {
+  userType: ""
+};
+
+/**
+ * 
+ * userType : 접속한 유저의 로그인 타입을 알기위한 State
+ *          : admin   - 관리자
+ *          : student - 학생
+ * 
+ */
+
+export default function reducer(state = initialState, action = {}) {
+    switch(action.type) {
+      case CHANGE_USER_TYPE:
+        return {
+          ...state,
+          userType: action.payload.userType
+        }
+      default:
+        return state
+    }
+  }
+
+export const changeUserType = userType => ({
+    type: CHANGE_USER_TYPE,
+    payload : {
+        userType
+    }
+});
