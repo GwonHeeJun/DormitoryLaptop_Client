132fd2bc4cd8c0e15f16cfa7f467e936a373e670
GwonHeeJun <gwonhj9751@naver.com>
2019-12-17 23:20:29 +0900

    [UPDATE] Use LifeCycle, DidMount SetState userTypeKor

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,20 +2,58 @@ import React, { Component } from "react";
 import { ReactComponent as NoteBook } from "assets/image/notebookPer.svg";
 import { ReactComponent as ConsultantImage } from "assets/image/management.svg";
 import { ReactComponent as ResidentImage } from "assets/image/resident.svg";
+import { connect } from "react-redux";
+import { changeAuthType } from "store/Menu/Menu.store";
 import "./Login.scss";
 
-export default class Login extends Component {
+class Login extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      userTypeKor: ""
+    };
+
+    this.onClickChangeAuthType = this.onClickChangeAuthType.bind();
   }
+
+  componentDidMount() {
+    const { title } = this.props;
+    switch (title) {
+      case "student":
+        this.setState({
+          userTypeKor: "학생"
+        });
+        break;
+      case "consultant":
+        this.setState({
+          userTypeKor: "자치위원"
+        });
+        break;
+      case "resident":
+        this.setState({
+          userTypeKor: "사감"
+        });
+        break;
+      default:
+        break;
+    }
+  }
+
+  onClickChangeAuthType = (e, authType) => {
+    const { changeAuthType } = this.props;
+    e.stopPropagation();
+
+    changeAuthType(authType);
+  };
 
   render() {
     return (
       <div className="c-login">
         <div className="c-login__form">
-          <h2 className="c-login__form--title">{this.props.title} 로그인</h2>
+          <h2 className="c-login__form--title">
+            {this.state.userTypeKor} 로그인
+          </h2>
 
           <form className="c-login__form--inputs">
             <input placeholder="이메일" />
@@ -38,7 +76,12 @@ export default class Login extends Component {
           </p>
           <p className="c-login__desc--explain">
             아직 계정이 없나요?
-            <span className="c-login__desc--explain__href">계정 생성하기</span>
+            <span
+              className="c-login__desc--explain__href"
+              onClick={e => this.onClickChangeAuthType(e, "register")}
+            >
+              계정 생성하기
+            </span>
           </p>
         </div>
         <div className="c-login__footer">
@@ -54,3 +97,11 @@ export default class Login extends Component {
     );
   }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    changeAuthType: authType => dispatch(changeAuthType(authType))
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Login);
