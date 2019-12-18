189cb7c0d481770fd1e0b5d79db273fe2a45bac8
GwonHeeJun <gwonhj9751@naver.com>
2019-12-18 18:17:08 +0900

    [UPDATE] Update Console.log Warning Message & Route HomeContainer path=/home

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import "./App.scss";
 
 // 컨테이너 모듈
 import MainContainer from "./containers/MainContainer/MainContainer";
+import HomeContainer from "containers/HomeContainer/HomeContainer";
 
 class App extends React.Component {
   constructor(props) {
@@ -19,6 +20,24 @@ class App extends React.Component {
 
   render() {
     const { store, history } = this.props;
+    var title = [
+      'font-size : 60px',
+      'color : red',
+      '-webkit-text-stroke: 1px black'
+    ].join(';');
+
+    var desc = [
+      'font-size : 18px',
+    ].join(';');
+
+    var warning = [
+      'font-size: 25px',
+      'color : red',
+
+    ].join(';');
+    console.log("%c 잠깐만!", title);
+    console.log("%c 이 기능은 개발자용으로 브라우저에서 제공되는 내용입니다.\n혹여나 누군가 여기에 뭐 치라고 하면 절-대-치-지-마-세-요.", desc);
+    console.log("%c 어택 날리면 지구 끝까지 추척한다.", warning);
     return (
       <Provider store={store}>
         <ConnectedRouter history={history}>
@@ -32,6 +51,7 @@ class App extends React.Component {
 const RoutesComponent = () => (
   <Fragment>
     <Route exact path="/" component={MainContainer} />
+    <Route exact path="/home" component={HomeContainer} />
   </Fragment>
 );
 
