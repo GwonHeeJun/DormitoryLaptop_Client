25cdf1686e88c4eb55952e70ef891debfae41892
GwonHeeJun <gwonhj9751@naver.com>
2019-12-19 17:58:22 +0900

    [CREATE] Create Morning Song Component

---
+++ src/components/MorningSong/MorningSong.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import "./MorningSong.scss";
+
+const dummyData = [
+    {},{},{},{},{},{},{}
+]
+
+export default class MorningSong extends Component {
+    render() {
+        return (
+            <div className="c-morning-song">
+                <div className="c-morning-song__header">
+                    <h2>기상음악</h2>
+                    <span>내일(12/17)</span>
+                </div>
+                <div className="c-morning-song__music-list">
+                    {dummyData.map((item, idx) => {
+                        return (
+                            <div className="c-morning-song__music-list--single">
+
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        )
+    }
+}
