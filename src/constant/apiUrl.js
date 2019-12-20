0205aa572ae4a7b44707ac7803e48c1d0b63a162
GwonHeeJun <gwonhj9751@naver.com>
2019-12-20 14:36:54 +0900

    [UPDATE] Update url, gsm

--- src/constant/apiUrl.js
+++ src/constant/apiUrl.js
@@ -1,11 +1,11 @@
 export const url = {
-  dasom:
+  gsm:
     process.env.NODE_ENV === "development"
       ? 'http://dormitory-management.us-east-2.elasticbeanstalk.com/'
       : 'http://10.120.74.181:4000/'
-}
+    }
 
 console.log(
   "this app is Service",
-  `${process.env.NODE_ENV.substr(0,2)}-${url.dasom.substr(21,4)}`
+  `${process.env.NODE_ENV.substr(0,2)}-${url.gsm.substr(21,4)}`
 )
