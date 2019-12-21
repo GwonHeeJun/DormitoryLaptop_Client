bcdeace041ba9c3d7a1ec963de1e87de1359dca4
GwonHeeJun <gwonhj9751@naver.com>
2019-12-21 23:55:47 +0900

    [UPDATE] Update Morning Song Width Error

--- src/components/MorningSong/MorningSong.js
+++ src/components/MorningSong/MorningSong.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "./MorningSong.scss";
 
-const dummyData = [
-    {},{},{},{},{},{},{}
-]
+const dummyData = [{}, {}, {}, {}, {}, {}, {}];
 
 export default class MorningSong extends Component {
-    render() {
-        return (
-            <div className="c-morning-song">
-                <div className="c-morning-song__header">
-                    <h2>기상음악</h2>
-                    <span>내일(12/17)</span>
-                </div>
-                <div className="c-morning-song__music-list">
-                    {dummyData.map((item, idx) => {
-                        return (
-                            <div className="c-morning-song__music-list--single" key={idx}>
-
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="c-morning-song">
+        <div className="c-morning-song__header">
+          <h2>기상음악</h2>
+          <span>내일(12/17)</span>
+        </div>
+        <div
+          style={{
+            overflowX: "auto",
+            overflowY: "hidden",
+            width: "1100px",
+            height: "90%",
+            paddingRight: "20px"
+          }}
+        >
+          <div className="c-morning-song__music-list">
+            {dummyData.map((item, idx) => {
+              return (
+                <div
+                  className="c-morning-song__music-list--single"
+                  key={idx}
+                ></div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
