04f06186200659bbda82963b26de0268ff50c4b1
GwonHeeJun <gwonhj9751@naver.com>
2019-12-22 14:32:17 +0900

    [CREATE] Create Setting Layout LaptopSeat Component

---
+++ src/components/LaptopSeat/LaptopSeat.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "./LaptopSeat.scss";
+
+export default class LaptopSeat extends Component {
+    render() {
+        return (
+            <div className="c-laptop-seat">
+                
+            </div>
+        )
+    }
+}
