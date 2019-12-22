30ced9adad1ab530f07e8bc82e080e53a56fc91d
GwonHeeJun <gwonhj9751@naver.com>
2019-12-22 14:32:26 +0900

    [CREATE] Create Setting Layout LaptopList Component

---
+++ src/components/LaptopList/LaptopList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "./LaptopList.scss";
+
+export default class LaptopList extends Component {
+    render() {
+        return (
+            <div className="c-laptop-list">
+                
+            </div>
+        )
+    }
+}
