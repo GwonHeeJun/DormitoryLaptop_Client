5fb514fbc1a452e835fcc19682965244bd7bb284
GwonHeeJun <gwonhj9751@naver.com>
2019-12-22 14:31:58 +0900

    [UPDATE] Update Connect Home, Laptop Containers

--- src/containers/HomeContainer/HomeContainer.js
+++ src/containers/HomeContainer/HomeContainer.js
@@ -1,39 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "./HomeContainer.scss";
-import NavigationBar from 'components/NavigationBar/NavigationBar';
-import ProfileBar from 'components/ProfileBar/ProfileBar';
-import ServiceBanner from 'components/ServiceBanner/ServiceBanner';
-import Notice from 'components/Notice/Notice';
-import MorningSong from 'components/MorningSong/MorningSong';
+import NavigationBar from "components/NavigationBar/NavigationBar";
+import ProfileBar from "components/ProfileBar/ProfileBar";
+import { connect } from "react-redux";
+import Home from "containers/Navigations/Home/Home";
+import Laptop from "containers/Navigations/Laptop/Laptop";
 
-export default class HomeContainer extends Component {
-    render() {
-        return (
-            <div className="home-container">
-                <div className="home-container__navigation">
-                    <NavigationBar />
-                </div>
-                <div className="home-container__content">
-                    <h2 className="home-container__content--title">홈 <small>2019년 12월 16일 월요일</small></h2>
-                    <div className="home-container__content--service">
-                        <ServiceBanner />
-                    </div>
-                    <div className="home-container__content--notice">
-                        <div className="consultant">
-                            <Notice title="자치 위원회"/>
-                        </div>
-                        <div className="resident">
-                            <Notice title="사감 선생님"/>
-                        </div>
-                    </div>
-                    <div className="home-container__content--song">
-                        <MorningSong />
-                    </div>
-                </div>
-                <div className="home-container__profile">
-                    <ProfileBar />
-                </div>
-            </div>
-        )
+class HomeContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  makeMainComponents() {
+    const { navigationType } = this.props;
+    switch (navigationType) {
+      case "home":
+        return <Home />
+      case "notice":
+        break;
+      case "laptop":
+        return <Laptop />
+      case "song":
+        break;
+      case "point":
+        break;
+      case "setting":
+        break;
+      case "report":
+        break;
+      default:
+        break;
     }
+  }
+
+  render() {
+    return (
+      <div className="home-container">
+        <div className="home-container__navigation">
+          <NavigationBar />
+        </div>
+        <div className="home-container__content">
+            {this.makeMainComponents()}
+        </div>
+        <div className="home-container__profile">
+          <ProfileBar />
+        </div>
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = state => {
+  return {
+    navigationType: state.menu.navigationType
+  };
+};
+
+export default connect(mapStateToProps, null)(HomeContainer);
