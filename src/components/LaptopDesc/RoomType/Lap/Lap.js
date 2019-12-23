a09b6bb334ff932fab7d1bf9a7696b4950a156b8
GwonHeeJun <gwonhj9751@naver.com>
2019-12-23 23:19:41 +0900

    [UPDATE] Update Setting Prettier, Fix Error

--- src/components/LaptopDesc/RoomType/Lap/Lap.js
+++ src/components/LaptopDesc/RoomType/Lap/Lap.js
@@ -1,14 +1,41 @@
 import React, { Component } from "react";
-import { ReactComponent as NoteBook } from 'assets/image/BnotebookSele.svg';
+import { ReactComponent as NoteBook } from "assets/image/BnotebookSele.svg";
 import "./Lap.scss";
 
 const dummyData = [
-  [{ seated : 0 }, { seated : 1 }, { seated : 0 }, { seated : 2 }, { seated : 2 }, { seated : 1 }],
-  [{ seated : 0 }, { seated : 0 }, { seated : 1 }, { seated : 2 }, { seated : 1 }, { seated : 0 }],
-  [{ seated : 0 }, { seated : 1 }, { seated : 0 }, { seated : 2 }, { seated : 2 }, { seated : 1 }],
-  [{ seated : 0 }, { seated : 0 }, { seated : 1 }, { seated : 2 }, { seated : 1 }, { seated : 0 }]
+  [
+    { seated: 0 },
+    { seated: 1 },
+    { seated: 0 },
+    { seated: 2 },
+    { seated: 2 },
+    { seated: 1 }
+  ],
+  [
+    { seated: 0 },
+    { seated: 0 },
+    { seated: 1 },
+    { seated: 2 },
+    { seated: 1 },
+    { seated: 0 }
+  ],
+  [
+    { seated: 0 },
+    { seated: 1 },
+    { seated: 0 },
+    { seated: 2 },
+    { seated: 2 },
+    { seated: 1 }
+  ],
+  [
+    { seated: 0 },
+    { seated: 0 },
+    { seated: 1 },
+    { seated: 2 },
+    { seated: 1 },
+    { seated: 0 }
+  ]
 ];
-
 
 export default class Lap extends Component {
   render() {
@@ -25,28 +52,53 @@ export default class Lap extends Component {
           <div className="c-lap__content--right">
             {dummyData.map((list, ix) => {
               return (
-                <div style={{ display: "flex", height: "60px", marginBottom: "20px" }}>
+                <div
+                  style={{
+                    display: "flex",
+                    height: "60px",
+                    marginBottom: "20px"
+                  }}
+                >
                   {list.map((item, idx) => {
-                      console.log(item)
+                    console.log(item);
                     if (idx <= 1) {
                       return (
-                        <div className={"c-lap__content--right__table-left" + `${item.seated === 0 ? "-not": "" }`}>
-                          {item.seated === 2 ? <NoteBook /> : ix * 6 + idx + 1 }
+                        <div
+                          className={
+                            "c-lap__content--right__table-left" +
+                            `${item.seated === 0 ? "-not" : ""}`
+                          }
+                        >
+                          {item.seated === 2 ? <NoteBook /> : ix * 6 + idx + 1}
                         </div>
                       );
                     } else if (idx === 2) {
                       return (
                         <React.Fragment>
                           <div style={{ marginRight: "85px" }} />
-                          <div className={"c-lap__content--right__table-left" + `${item.seated === 0 ? "-not": "" }`}>
-                          {item.seated === 2 ? <NoteBook /> : ix * 6 + idx + 1 }
+                          <div
+                            className={
+                              "c-lap__content--right__table-left" +
+                              `${item.seated === 0 ? "-not" : ""}`
+                            }
+                          >
+                            {item.seated === 2 ? (
+                              <NoteBook />
+                            ) : (
+                              ix * 6 + idx + 1
+                            )}
                           </div>
                         </React.Fragment>
                       );
                     } else {
                       return (
-                        <div className={"c-lap__content--right__table-left" + `${item.seated === 0 ? "-not": "" }`}>
-                          {item.seated === 2 ? <NoteBook /> : ix * 6 + idx + 1 }
+                        <div
+                          className={
+                            "c-lap__content--right__table-left" +
+                            `${item.seated === 0 ? "-not" : ""}`
+                          }
+                        >
+                          {item.seated === 2 ? <NoteBook /> : ix * 6 + idx + 1}
                         </div>
                       );
                     }
