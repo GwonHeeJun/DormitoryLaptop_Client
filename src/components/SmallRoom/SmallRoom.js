fd012c88cdee98e957308d350bb66486d1503c2a
GwonHeeJun <gwonhj9751@naver.com>
2019-12-26 16:13:17 +0900

    [UPDATE] Other Room

--- src/components/SmallRoom/SmallRoom.js
+++ src/components/SmallRoom/SmallRoom.js
@@ -11,17 +11,19 @@ export default class RoomDesc extends Component {
 
         }
     }
+
+
     
     render() {
         return (
             <div className="c-small-room">
                 <div className="c-small-room__list">
-                    <div className="self">
+                    <div className={5 === this.props.room ? "self__"+this.props.type : "self"}>
                         <span>자</span>
                     </div>
                     {dummy.map((item, idx) => {
                         return (
-                        <div className="lab">
+                        <div className="lab" key={idx}>
                             <div className={idx + 1 === this.props.room ? "lab__"+this.props.type : "lab__none"}>
                                 <span>{idx + 1}</span>
                             </div>
