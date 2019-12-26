2babd3fe29c127580b19a52b4ec27a23570be4f9
GwonHeeJun <gwonhj9751@naver.com>
2019-12-26 23:38:44 +0900

    [UPDATE] Login or Auth Teacher Handle

--- src/containers/Navigations/Laptop/Laptop.js
+++ src/containers/Navigations/Laptop/Laptop.js
@@ -3,7 +3,8 @@ import "./Laptop.scss";
 import ServiceBanner from "components/ServiceBanner/ServiceBanner";
 import LaptopList from "components/LaptopList/LaptopList";
 import LaptopDesc from "components/LaptopDesc/LaptopDesc";
-import { myLaptop } from "lib/laptop";
+import { myLaptop, roomDetail } from "lib/laptop";
+import AdminLaptop from "components/Admin/AdminLaptop/AdminLaptop";
 
 export default class Laptop extends Component {
   constructor(props) {
@@ -11,7 +12,8 @@ export default class Laptop extends Component {
 
     this.state = {
       myRoom: "",
-      mySeat: 0
+      mySeat: 0,
+      laptopDetail: []
     };
   }
 
@@ -21,25 +23,37 @@ export default class Laptop extends Component {
         this.setState({ myRoom: res.data.room, mySeat: res.data.seat })
       )
       .catch(err => console.log(err));
+
+      if (localStorage.getItem("authority") === "teacher") {
+        roomDetail().then(res => this.setState({
+          laptopDetail: res.data.seats
+        }))
+      }
   }
 
   render() {
     return (
       <React.Fragment>
         <h2 className="laptop-title">노트북 대여</h2>
-        <div className="laptop-content">
-          <div className="laptop-content__header">
-            <ServiceBanner banner="laptop" myRoom={this.state.myRoom} mySeat={this.state.mySeat}/>
+        {localStorage.getItem("authority") === "teacher" ? (
+          <div className="laptop-content">
+            <AdminLaptop roomDetail={this.state.laptopDetail}/>
           </div>
-          <div className="laptop-content__body">
-            <div className="laptop-content__body--list">
-              <LaptopList />
-            </div>
-            <div className="laptop-content__body--seat">
-              <LaptopDesc />
-            </div>
+        ) : <div className="laptop-content">
+        <div className="laptop-content__header">
+          <ServiceBanner banner="laptop" myRoom={this.state.myRoom} mySeat={this.state.mySeat}/>
+        </div>
+        <div className="laptop-content__body">
+          <div className="laptop-content__body--list">
+            <LaptopList />
+          </div>
+          <div className="laptop-content__body--seat">
+            <LaptopDesc />
           </div>
         </div>
+      </div>}
+        
+      
       </React.Fragment>
     );
   }
