c6e0fc626c0a9dfde6ff0c82aff03973fff957ba
GwonHeeJun <gwonhj9751@naver.com>
2019-12-26 23:38:17 +0900

    [UPDATE] Addtion Store

--- src/store/Laptop/Laptop.store.js
+++ src/store/Laptop/Laptop.store.js
@@ -1,7 +1,9 @@
 export const CHANGE_LAPTOP_ROOM = "CHANGE_LAPTOP_ROOM";
+export const CHANGE_ADMIN_LAPTOP_SELECT = "CHANGE_ADMIN_LAPTOP_SELECT";
 
 const initialState = {
   roomName: "lab1",
+  adminSelect: "all"
 };
 
 /**
@@ -12,6 +14,10 @@ const initialState = {
  *          : lab3   - 랩 3실
  *          : lab4   - 랩 4실
  *
+ * adminSelect : 관리자 유저의 대여대장 옵션 타입을 알기위한 State
+ *             : all     - 전체
+ *             : useful  - 이용가능
+ *             : caught  - 적발
  */
 
 export default function reducer(state = initialState, action = {}) {
@@ -20,6 +26,11 @@ export default function reducer(state = initialState, action = {}) {
         return {
           ...state,
           roomName: action.payload.roomName
+        }
+      case CHANGE_ADMIN_LAPTOP_SELECT:
+        return {
+          ...state,
+          adminSelect: action.payload.adminSelect
         }
       default:
         return state
@@ -31,4 +42,11 @@ export const changeLaptopRoom = roomName => ({
     payload : {
         roomName
     }
+});
+
+export const changeAdminLaptopSelect = adminSelect => ({
+  type: CHANGE_ADMIN_LAPTOP_SELECT,
+  payload : {
+      adminSelect
+  }
 });
